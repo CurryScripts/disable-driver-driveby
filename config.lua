d00420a11595111ccb1789f75dff297481865261
CurryScripts <117944456+CurryScripts@users.noreply.github.com>
2025-05-15 17:11:17 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Debug = false 
+Config.Debug = false -- Prints some important information in the client console!
 Config.CheckIntervall = 2 -- Seconds / How long should it take to detect the blacklist weapons [LOW NUMBER MEANS MORE PERFORMANCE CHANGE ON OWN RISK!]
 Config.WeaponList = { -- WHITELISTET WEAPONS / Weapons that ar not in this table are blacklist weapons and you cant use them as a driver! 
     "WEAPON_PISTOL",
